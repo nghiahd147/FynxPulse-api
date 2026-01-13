7940cbc9a479c53d5b4c8f1c70f2c3717442b345
nghiahd147 <qn500787@gmail.com>
2026-01-13 17:04:41 +0700

    feat: create user and hashtag collections and connect database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './services/connect'
+
+dotenv.config()
+connectDB()
 
 const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Home')
 })
 
-app.listen(3000, () => {
-  console.log('Server is running http://localhost:3000')
+app.listen(port, () => {
+  console.log(`Server is running http://localhost:${port}`)
 })
