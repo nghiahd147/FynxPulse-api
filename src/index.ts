ca1f25a397c68f3737bd65279eab8f2e5070d7eb
nghiahd147 <qn500787@gmail.com>
2026-01-18 17:11:36 +0700

    refactor: convert database connection to class-based implementation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/users.routes'
-import { run } from './services/database.services'
+import databaseServices from './services/database.services'
 
 dotenv.config()
-run().catch(console.dir)
+databaseServices.connect()
 
 const app = express()
 const port = process.env.PORT || 5000
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -3,13 +3,23 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const uri = process.env.MONGO_URI
-const client = new MongoClient(uri as string)
 
-export async function run() {
-  try {
-    await client.db('admin').command({ ping: 1 })
-    console.log('Pinged your deployment. You successfully connected to MongoDB!')
-  } finally {
-    await client.close()
+class DatabaseServices {
+  private client: MongoClient
+
+  constructor() {
+    this.client = new MongoClient(uri as string)
+  }
+
+  async connect() {
+    try {
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      await this.client.close()
+    }
   }
 }
+
+const databaseServices = new DatabaseServices()
+export default databaseServices
