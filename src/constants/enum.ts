1a6debb1a9c8a1d8314f5df8f88c6a763bf6bc3e
nghiahd147 <qn500787@gmail.com>
2026-02-03 16:28:50 +0700

    feat: implement password hashing on registration, add JWT, and create access token signing logic

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -8,3 +8,10 @@ export enum Role {
   admin,
   user
 }
+
+export enum TypeToken {
+  AcessToken,
+  RefreshToken,
+  EmailVerifyToken,
+  PasswordForgotToken
+}
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,7 +1,10 @@
 import { Filter } from 'mongodb'
+import { TypeToken } from '~/constants/enum'
 import { RegisterRequest } from '~/models/requests/users.requests'
 import User from '~/models/schemas/Users.schema'
 import databaseServices from '~/services/database.services'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
 
 type FiltesUser = Filter<User>
 
@@ -12,10 +15,23 @@ class UserServices {
     return result
   }
 
+  async signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        type_token: TypeToken.AcessToken
+      }
+    })
+  }
+
   async register(payload: RegisterRequest) {
-    const result = await databaseServices
-      .users()
-      .insertOne(new User({ ...payload, date_of_birth: new Date(payload.date_of_birth) }))
+    const result = await databaseServices.users().insertOne(
+      new User({
+        ...payload,
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
+      })
+    )
     return result
   }
 
